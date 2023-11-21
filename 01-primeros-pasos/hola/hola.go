61adc7ea5a2bd3e0ef35af610b4d7c601e4e064b
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-11-21 15:39:38 -0700

    control de errores

--- 01-primeros-pasos/hola/hola.go
+++ 01-primeros-pasos/hola/hola.go
@@ -2,6 +2,10 @@ package hola
 
 func Hola(name string) string {
 
+	if name == "" {
+		return "", error.new("El nombre esta vacios")
+	}
+
 	mensaje := "Hola, mi nombre es: " + name
 
 	return mensaje
