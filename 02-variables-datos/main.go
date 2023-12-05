cc3f42c990a96628e98d16ffe93eab7799bca695
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-12-05 09:14:13 -0700

    print format

--- 02-variables-datos/main.go
+++ 02-variables-datos/main.go
@@ -11,6 +11,22 @@ func main() {
 	fmt.Println("Welcome to the playground")
 	fmt.Println("The time is: ", time.Now())
 
-	fmt.Println("Now you have %g problem. \n", math.Sqrt(7))
+	var nombre string = "Hi iam"
 
+	fmt.Printf("Now you have %g problem. \n", math.Sqrt(7))
+	fmt.Printf("Now you have %v problem. \n", true)
+	fmt.Printf("Now you have %v problem. \n", nombre)
+	fmt.Printf("Now you have %#v problem. \n", nombre)
+	fmt.Printf("Now you have %q  problem. \n", nombre)
+
+	//The default format for %v is:
+	/*
+		bool:                    %t
+		int, int8 etc.:          %d
+		uint, uint8 etc.:        %d, %#x if printed with %#v
+		float32, complex64, etc: %g
+		string:                  %s
+		chan:                    %p
+		pointer:                 %p
+	*/
 }
