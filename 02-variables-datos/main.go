e1e70450735887fcd829e2143b770dedf967dea7
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-12-05 08:12:35 -0700

    Parte dos

---
+++ 02-variables-datos/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Welcome to the playground")
+	fmt.Println("The time is: ", time.Now())
+
+	fmt.Println("Now you have %g problem. \n", math.Sqrt(7))
+
+}
