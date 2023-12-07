f901272d40c78b9d4fd5ef7f52f2171242a40298
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-12-07 15:10:07 -0700

    estructura de if

--- 03-control-flujo/main.go
+++ 03-control-flujo/main.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
 	fmt.Println("Hi world")
 
-	basicFor()
-	ifElse()
+	//basicFor()
+	fmt.Printf(ifElse(4))
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 }
 
 func basicFor() {
 
 	var sum int
-
+	// for clasico normal
 	for i := 0; i < 10; i++ {
 		sum += i
 	}
 
 	sum = 0
-	// for continue
+	// for continue como si de un ciclo while se tratara
 	for sum < 100 { // el for continue es un while
 		sum += sum
 	}
@@ -34,6 +39,26 @@ func basicFor() {
 	fmt.Printf("The value sum is: %v", sum)
 }
 
-func ifElse() {
+func ifElse(x float64) string {
+
+	if x < 0 {
+		// llamas a la funcion para poner en negativo el valor de float
+		return ifElse(-x) + "i"
+	}
+
+	// Retornas un string format
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func pow(x, n, limit float64) float64 {
+
+	// Se puede delcarar las variable dentro del if y luego evaular esa
+	// misma variable
+
+	if v := math.Pow(x, n); v < limit {
+		return v
+	}
+
+	return limit
 
 }
