2d2678615b59e4f3d925ddcc750a5144bf1d4c6b
Milthon Borboa <81788252+MilthonB@users.noreply.github.com>
2023-12-07 15:39:56 -0700

    estructura switch

--- 03-control-flujo/main.go
+++ 03-control-flujo/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 func main() {
@@ -15,6 +17,9 @@ func main() {
 		pow(3, 2, 10),
 		pow(3, 3, 20),
 	)
+
+	loopFunction()
+	switchSentence()
 }
 
 func basicFor() {
@@ -57,8 +62,39 @@ func pow(x, n, limit float64) float64 {
 
 	if v := math.Pow(x, n); v < limit {
 		return v
+	} else { // Todas las variabls declaradas en los argumentos del if son disponibles en el bloque de else
+		fmt.Printf("%g >= %g \n", v, limit)
 	}
 
 	return limit
+
+}
+
+func switchSentence() {
+	fmt.Print("Go run on ")
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s. \n", os)
+	}
+
+	t := time.Now()
+	switch { // un switch sin argumento es un switch true
+	case t.Hour() > 12:
+		fmt.Println("Good morning")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon")
+	default:
+		fmt.Println("Good evenig")
+	}
+
+}
+
+// Exercise Loops and Functions
+func loopFunction() {
 
 }
